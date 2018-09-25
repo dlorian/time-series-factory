3c35d0ca88dbe2dcb407e9fad61d6c1ad9020e55
Florian Dorau <fdorau@it-economics.de>
2018-09-25 16:47:48 +0200

    Add CI/CD

--- src/index.js
+++ src/index.js
@@ -1,46 +1,52 @@
-const { parse } = require("./program.js");
+const { parse } = require('./program.js');
 
-const log = require("./logger.js");
+const log = require('./logger.js');
 const writer = require('./writer');
-const tsFactory = require("@dlorian/ts-factory");
+const tsFactory = require('@dlorian/ts-factory');
 
 const granularityMapping = {
-  "1d": "DAILY",
-  "1h": "HOURLY",
-  "15m": "QUARTER_HOURLY"
+    '1d': 'DAILY',
+    '1h': 'HOURLY',
+    '15m': 'QUARTER_HOURLY'
 };
 
 const process = input => {
-  let granularity = "DAILY";
-  if (input.granularity) {
-    granularity = granularityMapping[input.granularity];
-    if (!granularity) {
-      throw new Error(`Granularity ${granularity} not supported`);
+    let granularity = 'DAILY';
+    if (input.granularity) {
+        granularity = granularityMapping[input.granularity];
+        if (!granularity) {
+            throw new Error(`Granularity ${granularity} not supported`);
+        }
     }
-  }
 
-  const fileName = input.output || "test";
+    const fileName = input.output || 'test';
 
-  const tsOptions = {
-    start: input.start,
-    end: input.end,
-    format: input.format || "json",
-    dstMode: input.dst || "2424"
-  };
+    const tsOptions = {
+        start: input.start,
+        end: input.end,
+        format: input.format || 'json',
+        dstMode: input.dst || '2424'
+    };
 
-  log.info(
-    `You asked me to create a time series from '${tsOptions.start}' to '${tsOptions.end}' with a '${tsOptions.granularity}' granularity and DST correction '${tsOptions.dstMode}'. The time series will be creatd as '${tsOptions.format}' into '${fileName}.${tsOptions.format}'`
-  );
-
-  const tsStream = tsFactory.stream(tsOptions);
-
-  writer
-    .decorate(tsStream)
-    .write(`${fileName}.${tsOptions.format}`)
-    .then(() => log.info("Your time series has been created successfully."))
-    .catch(err =>
-      log.error("Something went wrong while creating your file. \n", err)
+    log.info(
+        `You asked me to create a time series from '${tsOptions.start}' to '${
+            tsOptions.end
+        }' with a '${tsOptions.granularity}' granularity and DST correction '${
+            tsOptions.dstMode
+        }'. The time series will be creatd as '${
+            tsOptions.format
+        }' into '${fileName}.${tsOptions.format}'`
     );
+
+    const tsStream = tsFactory.stream(tsOptions);
+
+    writer
+        .decorate(tsStream)
+        .write(`${fileName}.${tsOptions.format}`)
+        .then(() => log.info('Your time series has been created successfully.'))
+        .catch(err =>
+            log.error('Something went wrong while creating your file. \n', err)
+        );
 };
 
 process(parse(process.argv));
--- src/writer.js
+++ src/writer.js
@@ -1,27 +1,27 @@
-const log = require("./logger.js");
+const log = require('./logger.js');
 
-const { createWriteStream } = require("fs");
+const { createWriteStream } = require('fs');
 
 const write = function(fileName, stream) {
-  return new Promise((resolve, reject) => {
-    const ws = createWriteStream(`${fileName}`, {
-      flags: "w",
-      encoding: "utf8"
+    return new Promise((resolve, reject) => {
+        const ws = createWriteStream(`${fileName}`, {
+            flags: 'w',
+            encoding: 'utf8'
+        });
+
+        log.debug(`Writing data to file ${fileName}`);
+
+        stream
+            .on('finish', () => resolve())
+            .on('error', err => reject(err))
+            .pipe(ws);
     });
-
-    log.debug(`Writing data to file ${fileName}`);
-
-    stream
-      .on("finish", () => resolve())
-      .on("error", err => reject(err))
-      .pipe(ws);
-  });
 };
 
 const decorate = stream => {
-  return {
-    write: fileName => write(fileName, stream)
-  };
+    return {
+        write: fileName => write(fileName, stream)
+    };
 };
 
 module.exports = { decorate };
